e20a389fe0accdc75a49457576f2bf13379c8327
rs-tinnt1 <tinnt1@runsystem.net>
2024-06-26 17:17:07 +0700

    feat: implement admin role

--- database/migrations/2024_06_26_031448_create_roles_table.php
+++ database/migrations/2024_06_26_031448_create_roles_table.php
@@ -13,15 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            // 0=nuser, 1=admin, 2=oper
-            $table->tinyInteger('role')->default(0);
             $table->timestamps();
         });
     }
@@ -33,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
